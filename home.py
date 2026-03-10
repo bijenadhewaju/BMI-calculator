a5eb8b20e01f8c5c9e37ea160a9ea54d31cf5fd1
Bijena Dhewaju <bijenadhewaju181@gmail.com>
2026-03-10 21:29:07 +0545

    Task 1 commit, Date: 2026-03-10 task:Task 1: Ask user input and print it

--- home.py
+++ home.py
@@ -1,1 +1,5 @@
-print("hello world")
+# Task 1: Ask user input and print it
+name = input("What is your name? ")
+weight = input("What is your weight in kg? ")
+height = input("What is your height in meters? ")
+print("Your name is " + name + "\nyour weight is " + weight + " kg and, \nyour height is " + height + " meters.")
